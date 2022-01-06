bb1953191e7318694bce50b7ead714c44d4c32b7
Endri Dollani <endridollani99@gmail.com>
2022-01-06 01:46:02 +0100

    Successfully fetching from restAPI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+const UserProfiles = () => {
+
+  const [UserProfiles, setUserProfiles] = useState([]);
+
+  const fetchUserProfiles = () => {
+    axios.get("http://localhost:8080/api/v1/students").then(res => {
+      console.log(res);
+      setUserProfiles(res.data);
+    });
+  }
+
+  useEffect(()=>{
+    fetchUserProfiles();
+  }, []);
+
+  return  UserProfiles.map((userProfile, index) =>{
+    return (
+      <div key={index}>
+        <h1>{userProfile.firsName} {userProfile.lastName}</h1>
+        <h2>Email: {userProfile.email}</h2>
+        <h2>Gender: {userProfile.gender}</h2>
+        <h2>Country: {userProfile.address['country']}</h2>
+        <h2>City: {userProfile.address['city']}</h2>
+        <h2>Postal Code: {userProfile.address['postCode']}</h2>
+        <h2>Favorite Subjects: {userProfile.favoriteSubjects[0]}, {userProfile.favoriteSubjects[1]}</h2>
+        <h2>Total spent in books: {userProfile.totalSpentInBooks}</h2>
+        <h2>Created: {userProfile.created} Local Time</h2>
+
+      </div>
+    )
+  })
+}
 
 function App() {
   return (
-    <div><h1>Hello World!</h1></div>
+    <div className='App'>
+      <UserProfiles />
+    </div>
   );
 }
 
